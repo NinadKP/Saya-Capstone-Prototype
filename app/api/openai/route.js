c9479d78f01c97cd1e3a402fce25d812f82191f2
NinadKP <nkpyo97@gmail.com>
2024-03-04 02:24:07 -0800

    3.5 model test

--- app/api/openai/route.js
+++ app/api/openai/route.js
@@ -11,7 +11,7 @@ export async function POST(request) {
   console.log(reviewPrompt(body.data));
 
     const completion = await openai.chat.completions.create({
-    model: "gpt-4-0125-preview",
+    model: "gpt-3.5-turbo-0125",
     messages: [{"role": "user", "content": reviewPrompt(body.data)}],
   });
 
